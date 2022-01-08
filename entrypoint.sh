8f304a61d8e1b9e442bcf1645a456ce9e615a2ad
guzhongren <guzhongren@live.cn>
2022-01-08 10:15:14 +0800

    pref(shell): pref the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-npm install -g @algolia/cli
+inpm install -g @algolia/cli
 
 algolia import -s $FILE_PATH -a $APPLICATION_ID -k $ADMIN_API_KEY -n $INDEX_NAME 
+
+if [ "$?" != "0" ] ; then
+  echo "😢 Failed to upload your data to Algolia, PLZ report an issue, thx!"
+  exit 1
+fi
 
 echo "🚀 Successfully uploaded!"
