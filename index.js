d908ccb426612e576bdf848804dfebd9cba27a95
guzhongren <guzhongren@live.cn>
2022-10-30 16:03:46 +0000

    Update from https://github.com/guzhongren/algolia-docsearch-upload-action/commit/7697284a95258783c9a699c85620459f90fb666a

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-"use strict";
-exports.__esModule = true;
-var algoliasearch_1 = require("algoliasearch");
-var fs = require("fs");
-var process_1 = require("process");
+var algoliasearch = require('algoliasearch');
+var fs = require('fs');
+var exit = require('process').exit;
 var APPLICATION_ID = process.env.APPLICATION_ID;
 var ADMIN_API_KEY = process.env.ADMIN_API_KEY;
 var INDEX_NAME = process.env.INDEX_NAME;
 var FILE_PATH = process.env.FILE_PATH;
 if (!APPLICATION_ID && !ADMIN_API_KEY && !INDEX_NAME && !FILE_PATH) {
-    (0, process_1.exit)();
+    exit();
 }
-var client = (0, algoliasearch_1["default"])(APPLICATION_ID, ADMIN_API_KEY);
+var client = algoliasearch(APPLICATION_ID, ADMIN_API_KEY);
 var index = client.initIndex(INDEX_NAME);
 try {
     console.log("Start to save Objects");
